c42ae84f3503e326b94b6ebcebc7b37e82bbdbc9
Mitchell Cooper <cooper879@msn.com>
2013-05-17 19:52:50 -0400

    set_property and valid_value

--- Ferret.pm
+++ Ferret.pm
@@ -5,15 +5,45 @@ use warnings;
 use strict;
 use utf8;
 
+# object constants.
+my ($undefined, $true, $false) = (\'undefined', \'true', \'false');
+sub undefined () { $undefined }
+sub true      () { $true      }
+sub false     () { $false     }
+
 # create a new ferret.
 sub new {
     my ($class, %opts) = @_;
-    return bless \%opts, $class;
+    my $f = $bless {}, $class;
+    
+    # create the main context object.
+    $f->{context} = Ferret::Context->new($f, %opts);
+    
+    return $f;
 }
 
 # returns the main context.
 sub context {
     shift->{context};
+}
+
+# returns Perl boolean of whether or not a value is a valid Ferret value.
+sub valid_value {
+    my $value = shift;
+    
+    # if it's one of these non-object values, it's good.
+    if ($value == undefined || $value == true || $value == false) {
+        return 1;
+    }
+    
+    # if it's an object, it's good.
+    if (blessed($value) && $value->isa('Ferret::Object')) {
+        return 1;
+    }
+    
+    # it is none of these; not good.
+    return;
+    
 }
 
 1
--- Ferret/Object.pm
+++ Ferret/Object.pm
@@ -13,6 +13,56 @@ sub new {
     my $obj = bless { ferret => $f }, $class;
     return $obj;
 }
+##################
+### PROPERTIES ###
+##################
+
+# sets a property.
+# $obj->set_property(someProperty => $someValue)
+#
+# where $someValue is either
+#
+# a) a Ferret::Object (or something that inherits from Object)
+# b) a reference to code that returns a Ferret::Object
+#
+# returns Perl boolean true if set successfully, false otherwise.
+#
+sub set_property {
+    my ($obj, $prop_name, $value) = @_;
+    
+    # ensure that it is a valid Ferret value.
+    if (ref $value ne 'CODE' && !Ferret::valid_value($value)) {
+        return;
+    }
+    
+    # if we are setting it undefined, that just means we're deleting it.
+    if ($value == Ferret::undefined) {
+    
+        # XXX: delete method.
+        delete $obj->{properties}{$prop_name};
+        
+        return 1;
+    }
+    
+    # store the value.
+    $obj->{properties}{$prop_name} = $value;
+    
+    # good.
+    return 1;
+    
+}
+
+##########################
+### SPECIAL PROPERTIES ###
+##########################
+
+###################
+### INHERITANCE ###
+###################
+
+################
+### FETCHERS ###
+################
 
 # fetch the ferret.
 sub ferret {
