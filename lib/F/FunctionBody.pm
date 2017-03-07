41c2723c8ca58898165d175c82a8810495e18bf3
Mitchell Cooper <githubspam@rlygd.net>
2017-03-07 17:55:47 -0500

    fix confusing warning "function body" for on {}

--- lib/F/FunctionBody.pm
+++ lib/F/FunctionBody.pm
@@ -8,4 +8,12 @@ use 5.010;
 
 use parent 'F::Body';
 
+sub desc {
+    my $func = shift->parent;
+    my $func_type = $func->desc_type;
+    return 'on body'
+        if $func->parent->type eq 'On';
+    return "$func_type body";
+}
+
 1
