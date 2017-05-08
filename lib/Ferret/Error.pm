250f4a8f0ebdbfd13a441e2964b0a16952707d56
Mitchell Cooper <githubspam@rlygd.net>
2017-05-08 15:46:40 -0400

    setHint for errors

--- lib/Ferret/Error.pm
+++ lib/Ferret/Error.pm
@@ -9,13 +9,17 @@ use 5.010;
 use parent 'Ferret::Object';
 use Ferret::Core::Conversion qw(
     FUNC_V1 FUNC_SELF FUNC_ARGS FUNC_RET
-    fstring fnumber flist perlize
+    fstring fnumber fhash_fromref plist perlize
 );
 
 my @methods = (
     setPosition => {
         need => '$file:Str $line:Num',
         code => \&_setPosition
+    },
+    setHint => {
+        need => '$key:Str $value:Str',
+        code => \&_setHint
     }
 );
 
@@ -32,18 +36,31 @@ Ferret::bind_class(
 
 sub init {
     my ($err, $args) = &FUNC_V1;
+    $err->{hints} = {};
+    $err->{hint_keys} = [];
     $err->set_property($_ => $args->{$_})
-        for qw(type msg hints subError fatal);
-    $err->set_property(hints => flist()) if !$err->has_property('hints');
+        for qw(type msg subError fatal);
+    
+    # hints passed to constructor as a list
+    my @hints = plist($args->{hints});
+    while (my ($key, $val) = splice @hints, 0, 2) {
+        $err->set_hint($key, $val);
+    }
+        
+    $err->set_property(hints => fhash_fromref($err->{hints}));
     return $err;
 }
 
 sub _setPosition {
     my ($err, $args, $ret) = @_[FUNC_SELF, FUNC_ARGS, FUNC_RET];
-    $err->property('hints')->call_prop(push => [
-        fstring("File") => $args->{file},
-        fstring("Line") => $args->{line}
-    ]);
+    $err->set_hint(File => $args->{file});
+    $err->set_hint(Line => $args->{line});
+    return $ret;
+}
+
+sub _setHint {
+    my ($err, $args, $ret) = @_[FUNC_SELF, FUNC_ARGS, FUNC_RET];
+    $err->set_hint(@$args{'key', 'value'});
     return $ret;
 }
 
@@ -53,18 +70,26 @@ sub update_position {
     $err->call_prop(setPosition => [ fstring("$pos"), fnumber(int $pos) ]);
 }
 
+sub set_hint {
+    my ($err, $key, $value) = @_;
+    ($key, $value) = map perlize($_), $key, $value;
+    if (!exists $err->{hints}{$key}) {
+        push @{ $err->{hint_keys} }, $key;
+    }
+    $err->{hints}{$key} = $value;
+}
+
 sub description {
     my ($err, %opts) = @_;
     my ($type, $msg) = map $err->pstring($_), qw(type msg);
     my $fmt = "[$type] $msg";
 
     # error hints
-    my $hints = perlize($err->property('hints'), 1);
-    if (ref $hints eq 'ARRAY') {
-        $fmt .= "\n";
-        while (my ($hint, $val) = splice @$hints, 0, 2) {
-            $fmt .= "    $hint: $val\n";
-        }
+    my @hint_keys = @{ $err->{hint_keys} };
+    $fmt .= "\n" if @hint_keys;
+    foreach my $key (@hint_keys) {
+        my $val = $err->{hints}{$key};
+        $fmt .= "    $key: $val\n";
     }
 
     # sub error
